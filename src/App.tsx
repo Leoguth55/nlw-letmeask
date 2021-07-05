afcce2057c69a97dfe3dbd3acafb10bd600de5e4
Leonardo Guth <leoguth55@hotmail.com>
2021-07-05 12:32:15 +0100

    quase no final da aula 4

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { NewRoom } from './pages/NewRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/roons/new' component={NewRoom} />
-        <Route path='/roons/:id' component={Room} />
+          <Route path='/' exact component={Home} />
+          <Route path='/roons/new' component={NewRoom} />
+          <Route path='/roons/:id' component={Room} />
+          <Route path='/admin/roons/:id' component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
